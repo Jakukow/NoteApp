971aa371d7a5d18a7af37770c20f87ab676e6c9e
jakukow <jakubkowolik00@gmail.com>
2023-04-27 18:15:55 +0200

    Usuwanie

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Class
+
+class Note {
+  constructor(title, category, index) {
+    this.title = title;
+    this.category = category;
+    this.index = index;
+  }
+}
+const noteArr = [];
+let i = 0;
 //Selectors
 const mainDisplay = document.querySelector(".display");
 const overlay = document.querySelector(".overlay");
@@ -8,53 +19,98 @@ const closeButton = document.querySelector(".btnClose");
 const addNoteButton = document.querySelector(".newNote");
 const noteList = document.querySelector(".list-ul");
 const firstAddNoteButton = document.querySelector(".plus-icon");
+const title = document.querySelector(".title-Input");
+const category = document.querySelector(".cat");
+const bg = document.querySelector(".bg-active");
+const plus = document.querySelector(".plus");
 
 ///Markup
 
-html = `<li class="note-preview">
-<div class="note-block">
-  <div class="note">
-    <figure class="category-fig">
-      <img src="shopping.svg" />
-    </figure>
-    <div class="preview__data">
-      <h4 class="preview__title">ZAKUPY</h4>
-      <p class="preview__publisher">5 minut temu</p>
-    </div>
-  </div>
-  <div class="note">
-    Nam sagittis diam eu venenatis gravida. Pellentesque eget ex
-    mi.....
-  </div>
-  <div class="block-icons">
-    <img class="view" src="view-04.svg" />
-    <img class="view" src="edit.svg" />
-    <img class="view" src="trash.svg" />
-  </div>
-</div>
-</li>`;
-
 //Handlers
+noteList.addEventListener("click", function (e) {
+  if (!e.target.closest(".view")) return;
+  const key = e.target.closest(".view").id;
+  if (key === "remove") {
+    const id = e.target.parentNode.parentNode.parentNode.id;
+    const note1 = noteArr.findIndex((el) => String(el.index) === String(id));
+    e.target.parentNode.parentNode.parentNode.remove();
+    noteArr.splice(note1, 1);
+    if (noteArr.length === 0) {
+      FirstView();
+    }
+  }
+});
+
 newNoteButton.addEventListener("click", function () {
   toggleWindow();
 });
 overlay.addEventListener("click", function () {
-  toggleWindow();
+  overlay.classList.add("hidden");
+  newNoteDisplay.classList.add("hidden");
 });
 closeButton.addEventListener("click", function () {
   toggleWindow();
 });
 addNoteButton.addEventListener("click", function (e) {
+  i++;
   e.preventDefault();
-  RenderNewNote();
+  newNoteDisplay.classList.toggle("hidden");
+  const data = getDataNote();
+  title.value = "";
+  displayFormatting(data);
+});
+const displayFormatting = function (data) {
+  const note = new Note(...data, i);
+  noteArr.push(note);
+  FirstView();
+  RenderNewNote(...data);
+};
+firstAddNoteButton.addEventListener("click", function () {
   toggleWindow();
 });
-firstAddNoteButton.addEventListener("click", function () {});
 //New Note
 const toggleWindow = function () {
   overlay.classList.toggle("hidden");
   newNoteDisplay.classList.toggle("hidden");
 };
-const RenderNewNote = function () {
+const RenderNewNote = function (title, category) {
+  if (!category) {
+    category = `General`;
+  }
+
+  const html = `<li id='${i}' class="note-preview">
+  <div class="note-block">
+    <div class="note">
+      <figure class="category-fig">
+        <img src="${category}.svg" />
+      </figure>
+      <div class="preview__data">
+        <h4 class="preview__title">${title}</h4>
+        <p class="preview__publisher">${new Date(Date.now())
+          .toLocaleString()
+          .slice(0, -3)}</p>
+      </div>
+    </div>
+    <div class="note">
+      Nam sagittis diam eu venenatis gravida. Pellentesque eget ex
+      mi.....
+    </div>
+    <div class="block-icons">
+      <img id ="show" class="view" src="view-04.svg" />
+      <img id = "edit" class="view" src="edit.svg" />
+      <img id = "remove" class="view" src="trash.svg" />
+    </div>
+  </div>
+  </li>`;
   noteList.insertAdjacentHTML("afterbegin", html);
 };
+const getDataNote = function () {
+  return [title.value, category.value];
+};
+const FirstView = function () {
+  newNoteButton.classList.toggle("hidden");
+  plus.classList.toggle("hidden");
+  bg.classList.toggle("hidden");
+};
+FirstView();
+overlay.classList.toggle("hidden");
